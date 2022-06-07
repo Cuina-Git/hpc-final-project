2005cbca7908bee268c022c5f7076794f4cac353
Cuina-Git <12132388@mail.sustech.edu.cn>
2022-06-07 20:42:12 +0800

    Modify main.c

--- main.c
+++ main.c
@@ -11,10 +11,10 @@ int main(int argc,char **args)
    Mat            A;                /* linear system matrix */
    KSP            ksp;              /* linear solver context */
    PC             pc;               /* preconditioner context */
-   PetscReal      norm=0.0,norm_k=1.0,time,tol=1000.*PETSC_MACHINE_EPSILON;  /* norm of solution error */
+   PetscReal      time;             /* norm of solution error */
    PetscErrorCode ierr;
-   PetscInt       i,m = 100,n = 1000,col[3],rstart,rend,nlocal,rank;
-   PetscScalar    zero = 0.0,T = 1.0,value[3],delta_x = 0.01,delta_t = T/n,r=delta_t/(delta_x*delta_x);
+   PetscInt       i,m = 100,n = 1000,l = 1,col[3],rstart,rend,nlocal,rank,its;
+   PetscScalar    zero = 0.0,T = 1.0,value[3],ui,fi,delta_x = 0.01,delta_t = T/n,r=delta_t/(delta_x*delta_x);
 
    /* Initialize */
    ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
@@ -30,31 +30,34 @@ int main(int argc,char **args)
    /* Identify the starting and ending mesh points on each
       processor for the interior part of the mesh. We let PETSc decide
       above. */
-   ierr = VecGetOwnershipRange(y,&rstart,&rend);CHKERRQ(ierr);
-   ierr = VecGetLocalSize(z,&nlocal);CHKERRQ(ierr);
+   ierr = VecGetOwnershipRange(u,&rstart,&rend);CHKERRQ(ierr);
+   ierr = VecGetLocalSize(u,&nlocal);CHKERRQ(ierr);
 
    /* Set vector u & f */
    ierr = VecSet(u,zero);CHKERRQ(ierr);
    ierr = VecSet(f,zero);CHKERRQ(ierr);
    if (rank == 0){
       for (i = 0; i < m; i++) {
-         if (i == 0 || i == m - 1) {
+         if (i == 0 || i == m) {
 	         ierr = VecSetValues(u,1,&i,&zero,INSERT_VALUES);CHKERRQ(ierr);
          } 
          else {
-            u0   = exp(i*delta_x);
-            ierr = VecSetValues(u,1,&i,&u0,INSERT_VALUES);CHKERRQ(ierr);
+            ui   = exp(i*delta_x);
+            ierr = VecSetValues(u,1,&i,&ui,INSERT_VALUES);CHKERRQ(ierr);
          }
-         f0   = sin(l**i*delta_x);
-         ierr = VecSetValues(f,1,&i,&f0,INSERT_VALUES);CHKERRQ(ierr);
+         fi   = sin(l*PETSC_PI*i*delta_x);
+         ierr = VecSetValues(f,1,&i,&fi,INSERT_VALUES);CHKERRQ(ierr);
       }
+      ierr = VecScale(f,(PetscScalar)delta_t);CHKERRQ(ierr);
    }
 
    /* Assemble vector */
-   ierr = VecAssemblyBegin(z);CHKERRQ(ierr);
-   ierr = VecAssemblyEnd(z);CHKERRQ(ierr);
+   ierr = VecAssemblyBegin(u);CHKERRQ(ierr);
+   ierr = VecAssemblyEnd(u);CHKERRQ(ierr);
+   ierr = VecAssemblyBegin(f);CHKERRQ(ierr);
+   ierr = VecAssemblyEnd(f);CHKERRQ(ierr);
   
-   ierr = VecView(z,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+   ierr = VecView(u,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
    /* create matrix object */
    ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
@@ -89,28 +92,20 @@ int main(int argc,char **args)
    ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
    ierr = MatView(A,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
-   /* 迭代求解过程 */
-   while ( PetscAbsReal(norm-norm_k) > tol || its < max_it)
-   {
-      norm_k = norm;
-      ierr = MatMult(A,z,y);CHKERRQ(ierr);
-      ierr = VecNorm(y,NORM_2,&norm);CHKERRQ(ierr);
-      ierr = VecScale(y,one/norm);CHKERRQ(ierr);
-      ierr = VecCopy(y,z);CHKERRQ(ierr);
-      ierr = PetscPrintf(PETSC_COMM_WORLD,"Norm of error %g, Iterations %D\n",PetscAbsReal(norm-norm_k),its);CHKERRQ(ierr);
-      its++;
+   /* Slove the heat equation */
+    while (its < n){
+    ierr = MatMultAdd(A,u,f,u_new);CHKERRQ(ierr);
+    i = 0;
+    ierr = VecSetValues(u_new,1,&i,&zero,INSERT_VALUES);CHKERRQ(ierr);
+    i = m-1;
+    ierr = VecSetValues(u_new,1,&i,&zero,INSERT_VALUES);CHKERRQ(ierr);
+    ierr = VecCopy(u_new,u);CHKERRQ(ierr);
+    ierr = VecView(u,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+    its++;
    }
-   
-   ierr = VecView(z,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
-   /* 求解特征值 */
-   ierr = MatMultTranspose(A,z,T);CHKERRQ(ierr);
-   ierr = VecDot(T,z,&lamda);CHKERRQ(ierr);
-   ierr = PetscPrintf(PETSC_COMM_WORLD,"eigenvalue is %g\n",lamda);CHKERRQ(ierr);
-
-  
-   ierr = VecDestroy(&z);CHKERRQ(ierr); ierr = VecDestroy(&T);CHKERRQ(ierr); 
-   ierr = VecDestroy(&y);CHKERRQ(ierr); ierr = MatDestroy(&A);CHKERRQ(ierr);
+   ierr = VecDestroy(&u);CHKERRQ(ierr); ierr = VecDestroy(&u_new);CHKERRQ(ierr); 
+   ierr = VecDestroy(&f);CHKERRQ(ierr); ierr = MatDestroy(&A);CHKERRQ(ierr);
 
    ierr = PetscFinalize();
    return ierr;
