4c8f0a26c8528505c02051b2a343c5022ba7ed77
Cuina-Git <12132388@mail.sustech.edu.cn>
2022-06-07 20:00:01 +0800

    Modify main.c

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-static char help[] = "Solves a tridiagonal linear system.\n\n";
+static char help[] = "Solves r the transient heat equation in a one-dimensional \n\n";
 
 /*
   Include "petscksp.h" so that we can use KSP solvers. 
@@ -7,77 +7,87 @@ static char help[] = "Solves a tridiagonal linear system.\n\n";
 int main(int argc,char **args)
 {
 
-   Vec            y, z, T;          /* approx solution, RHS, exact solution */
+   Vec            u, u_new, f;      /* approx solution, RHS, exact solution */
    Mat            A;                /* linear system matrix */
    KSP            ksp;              /* linear solver context */
    PC             pc;               /* preconditioner context */
-   PetscReal      norm=0.0,norm_k=1.0,tol=1000.*PETSC_MACHINE_EPSILON;  /* norm of solution error */
+   PetscReal      norm=0.0,norm_k=1.0,time,tol=1000.*PETSC_MACHINE_EPSILON;  /* norm of solution error */
    PetscErrorCode ierr;
-   PetscInt       i,n = 10,col[3],its=0,rstart,rend,nlocal,rank,max_it=1000;
-   PetscScalar    zero = 0.0,one = 1.0,value[3],lamda,delta_x,delta_t;
+   PetscInt       i,m = 100,n = 1000,col[3],rstart,rend,nlocal,rank;
+   PetscScalar    zero = 0.0,T = 1.0,value[3],delta_x = 0.01,delta_t = T/n,r=delta_t/(delta_x*delta_x);
 
+   /* Initialize */
    ierr = PetscInitialize(&argc,&args,(char*)0,help);if (ierr) return ierr;
    ierr = PetscOptionsGetInt(NULL,NULL,"-n",&n,NULL);CHKERRQ(ierr);
 
-   /* 创建向量对象 */
-   ierr = VecCreate(PETSC_COMM_WORLD,&y);CHKERRQ(ierr);
-   ierr = VecSetSizes(y,PETSC_DECIDE,n);CHKERRQ(ierr);
-   ierr = VecSetFromOptions(y);CHKERRQ(ierr);
-   ierr = VecDuplicate(y,&z);CHKERRQ(ierr);
-   ierr = VecDuplicate(y,&T);CHKERRQ(ierr);
+   /* create vector object */
+   ierr = VecCreate(PETSC_COMM_WORLD,&u);CHKERRQ(ierr);
+   ierr = VecSetSizes(u,PETSC_DECIDE,m);CHKERRQ(ierr);
+   ierr = VecSetFromOptions(u);CHKERRQ(ierr);
+   ierr = VecDuplicate(u,&u_new);CHKERRQ(ierr);
+   ierr = VecDuplicate(u,&f);CHKERRQ(ierr);
 
-   /* 获得局部划分的上下界 */
+   /* Identify the starting and ending mesh points on each
+      processor for the interior part of the mesh. We let PETSc decide
+      above. */
    ierr = VecGetOwnershipRange(y,&rstart,&rend);CHKERRQ(ierr);
    ierr = VecGetLocalSize(z,&nlocal);CHKERRQ(ierr);
 
-   /* 创建矩阵对象 */
+   /* Set vector u & f */
+   ierr = VecSet(u,zero);CHKERRQ(ierr);
+   ierr = VecSet(f,zero);CHKERRQ(ierr);
+   if (rank == 0){
+      for (i = 0; i < m; i++) {
+         if (i == 0 || i == m - 1) {
+	         ierr = VecSetValues(u,1,&i,&zero,INSERT_VALUES);CHKERRQ(ierr);
+         } 
+         else {
+            u0   = exp(i*delta_x);
+            ierr = VecSetValues(u,1,&i,&u0,INSERT_VALUES);CHKERRQ(ierr);
+         }
+         f0   = sin(l**i*delta_x);
+         ierr = VecSetValues(f,1,&i,&f0,INSERT_VALUES);CHKERRQ(ierr);
+      }
+   }
+
+   /* Assemble vector */
+   ierr = VecAssemblyBegin(z);CHKERRQ(ierr);
+   ierr = VecAssemblyEnd(z);CHKERRQ(ierr);
+  
+   ierr = VecView(z,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
+
+   /* create matrix object */
    ierr = MatCreate(PETSC_COMM_WORLD,&A);CHKERRQ(ierr);
-   ierr = MatSetSizes(A,nlocal,nlocal,n,n);CHKERRQ(ierr);
+   ierr = MatSetSizes(A,nlocal,nlocal,m,m);CHKERRQ(ierr);
    ierr = MatSetFromOptions(A);CHKERRQ(ierr);
    ierr = MatSetUp(A);CHKERRQ(ierr);
 
-   /* 给矩阵元素赋值 */
+   /* Set matrix value */
    if (!rstart) 
    {
       rstart = 1;
-      i      = 0; col[0] = 0; col[1] = 1; value[0] = -2.0; value[1] = 1.0;
+      i      = 0; col[0] = 0; col[1] = 1; value[0] = 1.0-2.0*r; value[1] = r;
       ierr   = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
    }
   
-   if (rend == n) 
+   if (rend == m) 
    {
-      rend = n-1;
-      i    = n-1; col[0] = n-2; col[1] = n-1; value[0] = 1.0; value[1] = -2.0;
+      rend = m-1;
+      i    = m-1; col[0] = m-2; col[1] = n-1; value[0] = r; value[1] = 1.0-2.0*r;
       ierr = MatSetValues(A,1,&i,2,col,value,INSERT_VALUES);CHKERRQ(ierr);
    }
 
-   value[0] = 1.0; value[1] = -2.0; value[2] = 1.0;
+   value[0] = r; value[1] = 1.0-2.0*r; value[2] = r;
    for (i=rstart; i<rend; i++) 
    {
       col[0] = i-1; col[1] = i; col[2] = i+1;
       ierr   = MatSetValues(A,1,&i,3,col,value,INSERT_VALUES);CHKERRQ(ierr);
    }
 
-   /* 矩阵集聚 */
+   /* Assemble matrix */
    ierr = MatAssemblyBegin(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
    ierr = MatAssemblyEnd(A,MAT_FINAL_ASSEMBLY);CHKERRQ(ierr);
-
    ierr = MatView(A,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
-  
-   /*
-      给向量z赋值
-   */
-   ierr = VecSet(z,zero);CHKERRQ(ierr);
-   if (rank == 0){
-      i    = 0;
-      ierr = VecSetValues(z,1,&i,&one,INSERT_VALUES);CHKERRQ(ierr);
-   }
-
-   /* 向量集聚 */
-   ierr = VecAssemblyBegin(z);CHKERRQ(ierr);
-   ierr = VecAssemblyEnd(z);CHKERRQ(ierr);
-  
-   ierr = VecView(z,PETSC_VIEWER_STDOUT_WORLD);CHKERRQ(ierr);
 
    /* 迭代求解过程 */
    while ( PetscAbsReal(norm-norm_k) > tol || its < max_it)
