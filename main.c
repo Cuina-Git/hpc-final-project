592d9cb5b21a5c8dbed60964548aa767c8746212
Cuina-Git <12132388@mail.sustech.edu.cn>
2022-06-09 01:07:49 +0800

    Modify main.c

--- main.c
+++ main.c
@@ -158,7 +158,7 @@ int main(int argc,char **args)
 
    ierr = VecDestroy(&u);CHKERRQ(ierr); ierr = VecDestroy(&u_new);CHKERRQ(ierr); 
    ierr = VecDestroy(&f);CHKERRQ(ierr); ierr = MatDestroy(&A);CHKERRQ(ierr);
-   ierr = VecDestroy(&init);CHKERRQ(ierr);
+   ierr = VecDestroy(&init);CHKERRQ(ierr); ierr = PetscViewerDestroy(&viewer);CHKERRQ(ierr);
 
    ierr = PetscFinalize();
    return ierr;
